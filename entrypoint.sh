729c6cbc10ba35ad871b02a9414dfcb43ef5a004
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2023-05-30 17:13:11 +0300

    Entrypoint fix to work in containers built with Yocto

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-source /opt/ros/humble/setup.bash
-
 echo "Starting the RTK GPS client (ntrip_client)"
 
 mkdir -p ~/.ros/log
 
-ros2 launch ntrip_client ntrip_client_launch.py
+ros-with-env ros2 launch ntrip_client ntrip_client_launch.py
